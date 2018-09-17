ec05a914fd1c584cfa18a4758947a9712fc82d31
Max Dragaytsev <dmax1447@gmail.com>
2018-09-17 20:36:43 +0300

    вынес переменные наверх, коды клавиш в utils.js,переписал условие на обработчике

--- js/setup.js
+++ js/setup.js
@@ -10,8 +10,13 @@
   var setupOpenIcon = document.querySelector('.setup-open');
   var setupCloseIcon = setupWindow.querySelector('.setup-close');
   var setupWizard = document.querySelector('.setup-wizard');
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
+  var wizardCoat = setupWizard.querySelector('.wizard-coat');
+  var inputCoatColor = document.querySelector('input[name="coat-color"]');
+  var setupFireball = document.querySelector('.setup-fireball-wrap');
+  var inputFireball = document.querySelector('input[name="fireball-color"]');
+  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+  var inputEyesColor = document.querySelector('input[name="eyes-color"]');
+  var userNameInput = document.querySelector('.setup-user-name');
 
   // функции открытия и закрытия окна setup
   var openSetupWindow = function () {
@@ -19,6 +24,13 @@
   };
   var closeSetupWindow = function () {
     setupWindow.classList.add('hidden');
+  };
+
+  // функция для изменения цвета элемента волшебника и соответствующего инпута
+  var setColor = function (domElement, domElementProperty, colors, inputElement) {
+    var color = window.utils.getRandomElement(colors);
+    domElement.style[domElementProperty] = color;
+    inputElement.value = color;
   };
 
   // добавляем обработчик: открытие окна по клику на иконку пользователя
@@ -33,52 +45,41 @@
 
   // добавляем обработчик: открытие окна по нажатию ENTER
   setupOpenIcon.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
+    if (evt.keyCode === window.utils.ENTER_KEYCODE) {
       openSetupWindow();
     }
   });
 
   // добавляем обработчик: закрытие окна ESC при условии что поле ввода имени не в фокусе
   document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ESC_KEYCODE && !(document.activeElement === userNameInput)) {
+    if (evt.keyCode === window.utils.ESC_KEYCODE && document.activeElement !== userNameInput) {
       closeSetupWindow();
     }
   });
 
   // добавляем обработчик: закрытие окна по ENTER когда в фокусе иконка крестик
   setupCloseIcon.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
+    if (evt.keyCode === window.utils.ENTER_KEYCODE) {
       closeSetupWindow();
     }
   });
 
-  // функция для изменения цвета элемента волшебника и соответствующего инпута
-  var setColor = function (domElement, domElementProperty, colors, inputElement) {
-    var color = window.utils.getRandomElement(colors);
-    domElement.style[domElementProperty] = color;
-    inputElement.value = color;
-  };
   // добавляем обработчик: по клику на плащ меняем цвет плаща и значение цвета в инпуте
-  var wizardCoat = setupWizard.querySelector('.wizard-coat');
-  var inputCoatColor = document.querySelector('input[name="coat-color"]');
   wizardCoat.addEventListener('click', function () {
     setColor(wizardCoat, 'fill', window.utils.coatColors, inputCoatColor);
   });
+
   // добавляем обработчик: по клику на глаза меняем цвет глаз и значение цвета в инпуте
-  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
-  var inputEyesColor = document.querySelector('input[name="eyes-color"]');
   wizardEyes.addEventListener('click', function () {
     setColor(wizardEyes, 'fill', window.utils.eyesColors, inputEyesColor);
   });
+
   // добавляем обработчик: по клику на глаза меняем цвет глаз и значение цвета в инпуте
-  var setupFireball = document.querySelector('.setup-fireball-wrap');
-  var inputFireball = document.querySelector('input[name="fireball-color"]');
   setupFireball.addEventListener('click', function () {
     setColor(setupFireball, 'background', window.utils.fireballColors, inputFireball);
   });
 
   // обработка ошибок при вводе имени, добавим сообщения на русском
-  var userNameInput = document.querySelector('.setup-user-name');
   userNameInput.addEventListener('invalid', function () {
     if (userNameInput.validity.tooShort) {
       userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
--- js/utils.js
+++ js/utils.js
@@ -9,6 +9,8 @@
     coatColors: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     eyesColors: ['black', 'red', 'blue', 'yellow', 'green'],
     fireballColors: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
     getRandomElement: function (array) {
       var index = Math.floor(array.length * Math.random());
       return array[index];
