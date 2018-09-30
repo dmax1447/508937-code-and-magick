0281ebce594c4317d893c659d10819197e147498
Max Dragaytsev <dmax1447@gmail.com>
2018-09-30 20:50:42 +0300

    сделал отрисовку похожих, без дебонса

--- js/setup.js
+++ js/setup.js
@@ -18,7 +18,7 @@
   var userNameInput = document.querySelector('.setup-user-name');
   var form = document.querySelector('.setup-wizard-form');
   var btnSubmit = document.querySelector('.setup-submit'); // кнопка
-
+  var wizardList = document.querySelector('.setup-similar-list'); // список похожих волшеюников
   // функции открытия и закрытия окна setup
   var openSetupWindow = function () {
     setupWindow.classList.remove('hidden');
@@ -27,11 +27,22 @@
     setupWindow.classList.add('hidden');
   };
 
-  // функция для изменения цвета элемента волшебника и соответствующего инпута
+  // коллбек по изменению цвета элементов персонажа
   var setColor = function (domElement, domElementProperty, colors, inputElement) {
-    var color = window.utils.getRandomElement(colors);
-    domElement.style[domElementProperty] = color;
-    inputElement.value = color;
+    var color = window.utils.getRandomElement(colors); // берем случайный цвет из полученного диапазона
+    domElement.style[domElementProperty] = color; // меняем цвет заданного элемента на сгенерированный
+    inputElement.value = color; // обновляем значение соответствующего инпута
+    window.setup.currentWizard.colorEyes = inputEyesColor.value; // сохраняем цвета инпутов
+    window.setup.currentWizard.colorCoat = inputCoatColor.value;
+    window.setup.currentWizard.colorFireball = inputFireball.value;
+    window.setup.wizardsData.forEach(window.simwiz.addSimilarityData); // рассчитываем похожесть по сохранным данным о инпутах
+    window.setup.wizardsData.sort(window.simwiz.sortBySimilarity); // сортируем по перерасчитанному критерию похожести
+    var newWizards = window.simwiz.renderList(window.setup.wizardsData); // рендерим новый список похожих и сохраяем в фрагмент
+    while (wizardList.firstChild) { // удаляем все дочерние дом ноды из списка похожих волшебников
+      wizardList.removeChild(wizardList.firstChild);
+    }
+    wizardList.appendChild(newWizards); // заполняем список новыми
+
   };
 
   // обработчик клика по кнопке сохранить
@@ -103,5 +114,16 @@
   // добавляю обработчик нажатия на кнопку сохранить
   btnSubmit.addEventListener('click', onBtnSubmitClick);
 
-})();
 
+
+  // экспортирую данные о параметрах текущего волшебника
+  window.setup = {
+    currentWizard: {
+      name: userNameInput.value,
+      colorEyes: inputEyesColor.value,
+      colorCoat: inputCoatColor.value,
+      colorFireball: inputFireball.value
+    },
+    wizardData: [],
+  };
+})();
--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -7,6 +7,8 @@
 
   var wizardList = document.querySelector('.setup-similar-list'); // находим список волшебников сохраняем в wizardList
   var setupSimilar = document.querySelector('.setup-similar'); // блок похожих волшебников
+  // window.similarwizards = [];
+
 
   // функция для отрисовки волшебника. Вернет сгенерированный DOM элемент согласно шаблону и  переданным данным в виде JS объекта
   var renderWizard = function (wizardData) {
@@ -21,17 +23,56 @@
     return wizardElement;
   };
 
-  // коллбек для отрисовки списка похожих волшебников при успешной загрузке
-  var renderList = function (wizardsData) {
+  // функция расчета критерия похожести
+  var addSimilarityData = function (wizardData) {
+    var currentWizard = window.setup.currentWizard;
+    wizardData.similarity = 0;
+    if (currentWizard.colorEyes === wizardData.colorEyes) {
+      wizardData.similarity++;
+    }
+    if (currentWizard.colorCoat === wizardData.colorCoat) {
+      wizardData.similarity += 2;
+    }
+  };
+
+  // функция сортировки в обратном порядке по полю "похожесть"
+  var sortBySimilarity = function (first, second) {
+    if (first.similarity < second.similarity) {
+      return 1;
+    } else if (first.similarity > second.similarity) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  // коллбек для вызова по загрузке, принимает загруженные данные
+  var onListLoad = function (data) {
+    window.setup.wizardsData = data.slice(0); // создаем копию загруженного массива данных волшебников
+    data.forEach(addSimilarityData); // обрабатываем данные: рассчитываем для каждого волшебника похожесть
+    data.sort(sortBySimilarity); // сортируем данные по критерию похожести
+    var wizardsNodeList = renderList(data); // рендерим в фрагмент волшебников по обработнным данным
+    wizardList.appendChild(wizardsNodeList); // вставляем в список похожих волшебников готовый фрагмент
+    setupSimilar.classList.remove('hidden'); // показываем список похожих волшебников в блоке
+  };
+
+  // функция возвращает htmlFragment с отрисованными волшебниками, на входе массив данных о волшебниках
+  var renderList = function (data) {
     var fragment = document.createDocumentFragment(); // создаем пустой фрагмент
     for (var i = 0; i < 4; i++) { // циклом по массиву данных о волшебниках
-      var wizard = renderWizard(wizardsData[i]); // создаем элемент по данным
+      var wizard = renderWizard(data[i]); // создаем элемент по данным
       fragment.appendChild(wizard); // вставляем сгенерированный по данным элемент(волшебника) в пустой фрагмент
     }
-    wizardList.appendChild(fragment);
-    setupSimilar.classList.remove('hidden'); // показываем блок похожих волшебников
+    return fragment;
+  };
+
+  // экспортируем функции
+  window.simwiz = {
+    renderList: renderList,
+    sortBySimilarity: sortBySimilarity,
+    addSimilarityData: addSimilarityData
   };
 
   // пытаемся загрузить волшебников, если удачно - то рендерим в список, если нет то выводим сообщение об ошибке
-  window.backend.loadSimilarWizards(renderList, window.utils.showError);
+  window.backend.loadSimilarWizards(onListLoad, window.utils.showError);
 })();
